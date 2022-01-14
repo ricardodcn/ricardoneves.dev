93e590eb1139128801042b76940fe60669e3570b
ricardodcn <ricardodecarvalhoneves@gmail.com>
2022-01-14 00:11:20 -0300

    pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicialComponent } from './inicial/inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicialComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: InicialComponent },
+    ])
+    
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
